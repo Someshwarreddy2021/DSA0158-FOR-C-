817a6c1a9b973f9cd68f234da22f4cd306e4128e
Someshwarreddy2021 <141160527+Someshwarreddy2021@users.noreply.github.com>
2024-07-23 12:42:37 +0530

    Add files via upload

---
+++ exam1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+   
+    int x = 10;  
+    int y = 20;  
+   
+    const int PI = 3.14;  
+
+    
+    int sum = x + y;  
+   
+    std::cout << "The sum is: " << sum << std::endl;
+
+    return 0;
+}
+
