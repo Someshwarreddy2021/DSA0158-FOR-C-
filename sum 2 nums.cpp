ab39645dfc7c288e9b12b709acc191d9c9ab9f85
Someshwarreddy2021 <141160527+Someshwarreddy2021@users.noreply.github.com>
2024-07-23 12:36:10 +0530

    Add files via upload

---
+++ sum 2 nums.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,sum;
+	cout<<"Enter the a value:";
+	cin>>a;
+	cout<<"Enter the b value:";
+	cin>>b;
+	
+	sum=a+b;
+	cout<<"The sum is:",cout<<sum;
+	
+	return 0;
+}
